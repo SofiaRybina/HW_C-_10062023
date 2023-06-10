0338f5b93774e98ac3f27ce4b752a3040eebdef2
Sofia <rybina.sofia@gmail.com>
2023-06-10 23:20:16 +0300

    task_004_three_num_max

---
+++ task_004_three_num_max/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.WriteLine("Write the first number:");
+int num1 = int.Parse(Console.ReadLine())!;
+Console.WriteLine("Write the second number:");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Write the third number:");
+int num3 = int.Parse(Console.ReadLine()!);
+int max = num1;
+
+if(num2 > max && num2 > num3){
+    max = num2;
+    Console.WriteLine("Result:");
+    Console.WriteLine($"{num1},{num2},{num3} -> {max}");
+    }
+    else if(num3 > max && num3 > num2){
+        max = num3;
+        Console.WriteLine("Result:");
+        Console.WriteLine($"{num1},{num2},{num3} -> {max}");}
+        else{
+            Console.WriteLine("Result:");
+            Console.WriteLine($"{num1},{num2},{num3} -> {max}");
+            }
